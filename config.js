51d7122e82a7146356ba8e9158f05d604656d5e6
rillasus06-ctrl <rillasus06@gmail.com>
2026-05-31 22:02:21 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js — add this file to .gitignore, never commit it
-const CONFIG = {
-  TMDB_KEY: 'be70b644fd5b6c8bf3ff3e949bde6db4',
-  TMDB_BASE: 'https://api.themoviedb.org/3',
-  TMDB_IMG:  'https://image.tmdb.org/t/p/w500',
-};
